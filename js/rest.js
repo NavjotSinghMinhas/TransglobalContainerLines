0abf733c910eed090ecf0780fc695ea4428429ec
Navjot Singh Minhas <navjotsingh548@gmail.com>
2020-07-22 17:32:58 +0530

    Update rest.js

--- js/rest.js
+++ js/rest.js
@@ -10,7 +10,7 @@ async function getData(url = '') {
 async function postData(url = '', data = {}) {
     var response = await fetch(url, {
         method: 'POST',
-        mode: 'no-cors', // no-cors, *cors, same-origin
+        mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache',
         body: data
     });
